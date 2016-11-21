141d7f3a7228a3bdf621dc4351f3a91278cbb265
Skyllarr <skyllarr@gmail.com>
2016-11-21 19:01:36 +0100

    all done - icon, styles, button to change theme and main activity

---
+++ app/src/main/java/cz/muni/fi/pv256/movio2/uco374585/MyAdapter.java
@@ -0,0 +1,48 @@
+package cz.muni.fi.pv256.movio2.uco374585;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
+    private MyData[] itemsData;
+
+    public MyAdapter(MyData[] itemsData) {
+        this.itemsData = itemsData;
+    }
+
+    @Override
+    public MyAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemLayoutView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.list_cell, null);
+
+        ViewHolder viewHolder = new ViewHolder(itemLayoutView);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder viewHolder, int position) {
+        viewHolder.txtViewTitle.setText(itemsData[position].getTitle());
+        viewHolder.imgViewIcon.setImageResource(itemsData[position].getImageUrl());
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+
+        public TextView txtViewTitle;
+        public ImageView imgViewIcon;
+
+        public ViewHolder(View itemLayoutView) {
+            super(itemLayoutView);
+            txtViewTitle = (TextView) itemLayoutView.findViewById(R.id.item_title);
+            imgViewIcon = (ImageView) itemLayoutView.findViewById(R.id.item_image);
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return itemsData.length;
+    }
+}
