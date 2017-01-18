a5456b8e8cf463a8aa82c7ca490ff3729e64262b
Skyllarr <skyllarr@gmail.com>
2017-01-18 13:01:20 +0100

    movieDownloader to be used only once, URL addresses refactored

--- app/src/main/java/cz/muni/fi/pv256/movio2/uco374585/Api/ApiQuery.java
+++ app/src/main/java/cz/muni/fi/pv256/movio2/uco374585/Api/ApiQuery.java
@@ -10,21 +10,8 @@ import java.util.GregorianCalendar;
  */
 
 public class ApiQuery {
-    public static final String API_KEY = "&api_key=01f5cc7d21cfb631240703a6c7cb2790";
-    public static final String DISCOVER_URL = "https://api.themoviedb.org/3/discover/movie";
+
+    public static final String API_KEY = "01f5cc7d21cfb631240703a6c7cb2790";
+    public static final String TMDB_URL = "api.themoviedb.org";
     public static final String IMAGE_URL = "https://image.tmdb.org/t/p/original/";
-    public static final String THIS_WEEK_URL = "?primary_release_date.gte=" + todayDate() +
-            "&primary_release_date.lte=" + weekFromToday();
-    public static final String MOST_POPULAR_EVER_URL = "?sort_by=popularity.desc";
-    public static final String MOST_POPULAR_THIS_YEAR_URL = "?primary_release_year=2017&sort_by=vote_average.desc";
-
-    private static String todayDate() {
-        return new SimpleDateFormat("yyyy-MM-dd").format(new Date());
-    }
-
-    private static String weekFromToday() {
-        Calendar calendar = new GregorianCalendar();
-        calendar.add(Calendar.DATE, 7);
-        return new SimpleDateFormat("yyyy-MM-dd").format(calendar.getTime());
-    }
 }
